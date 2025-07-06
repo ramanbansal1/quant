b9af66aa99c7b20f1b600f6f84be69c36d152c4d
Raman Bansal <bansalraman339@gmail.com>
2025-07-06 17:55:01 +0530

    Update layout

--- BlackScholes/app.py
+++ BlackScholes/app.py
@@ -74,10 +74,12 @@ with col2:
     st.container(border=True).metric(label='Put option', value=put_option)
 
 st.subheader("Option Price Heatmap")
-col1, col2 = st.columns(2)
 
+
+
+col1, col2 = st.columns(2)
 # Create heatmap data
-spot_range = np.linspace(S - 30, S + 30, 50)
+spot_range = np.linspace(max(1e-5, S - 30), S + 30, 50)
 vol_range = np.linspace(0.1, 1.0, 50)
 
 call_prices = np.array([[black_scholes(s, K, T, r, v, 'call') for v in vol_range] for s in spot_range])
@@ -86,12 +88,12 @@ with col1:
     fig_call, ax1 = plt.subplots()
     c1 = ax1.imshow(call_prices, cmap='viridis', aspect='auto', 
                     extent=[vol_range[0], vol_range[-1], spot_range[0], spot_range[-1]],
-                    origin='lower')
+                    origin='lower',)
     fig_call.colorbar(c1, ax=ax1, label="Call Price")
     ax1.set_title("Call Option Heatmap")
     ax1.set_xlabel("Volatility (σ)")
     ax1.set_ylabel("Spot Price (S)")
-    ax1.grid(linewidth=2, linestyle='-', alpha=.5, color='black')
+    ax1.grid(linewidth=1, linestyle='-', alpha=.5, color='black',)
     st.pyplot(fig_call)
 
 with col2:
@@ -103,4 +105,6 @@ with col2:
     ax2.set_title("Put Option Heatmap")
     ax2.set_xlabel("Volatility (σ)")
     ax2.set_ylabel("Spot Price (S)")
+    ax2.grid(linewidth=2, linestyle='-', alpha=.5, color='black')
     st.pyplot(fig_put)
+
